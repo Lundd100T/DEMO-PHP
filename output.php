0d6513867f6cb5096846e88bcc7136789143976a
Lundd100T <garlan.lomongo@sdca.edu.ph>
2024-10-04 23:34:41 -0700

    Update output.php

--- output.php
+++ output.php
@@ -2,43 +2,88 @@
 
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-   $email               = $_POST["email"];
    $username            = $_POST["username"];
+   $email               = $_POST["email"];
    $password            = $_POST["password"];
    $confirm_password    = $_POST["confirm-password"];
 
 
+   
+   
 
 
-   if (trim($email) != ""){
-    echo "User email is not empty <br>";
+
+
+
+
+    //Validate
+   
+    if (trim($username) != ""){
+    echo "User username is not empty<br>";
+    } else {
+    echo "Field username is empty <br>";
+    }
+
+
+    if (trim($email) != ""){
+    echo "User email is not empty<br>";
     } else {
     echo "Field email is empty <br>";
     }
 
-/*
-   if (trim($username) != ""){
-        echo "User username is not empty <br>";
-   } else {
-    echo "Field username is empty <br>";
-   }
 
-
-   if (trim($password) != ""){
-    echo "User password is not empty <br>";
+    if (trim($password) != ""){
+    echo "User password is not empty<br>";
     } else {
     echo "Field password is empty <br>";
     }
 
 
-   if (trim($confirm_password) != ""){
-        echo "User confirm password is not empty <br>";
-   } else {
+    if (trim($confirm_password) != ""){
+    echo "User confirm password is not empty<br>";
+    } else {
     echo "Field password is empty <br>";
-   }
-//echo "User email is ". $email. "<br>";
-// echo "User username is ". $username. "<br>";
-*/
+    }
+   
+    if($password == $confirm_password){
+
+
+        header("location: demo1.php?success=REGISTRATION SUCCESSFUL");
+        exit;
+
+
+    } else {
+        header("location: demo1.php?error=PASSWORD MISMATCH");
+        exit;
+
+
+    }
+
+
 }
 
+
+
+
+
+
+   
+//echo "User email is ". $email. "<br>";
+// echo "User username is ". $username. "<br>";
+ /*test($username);
+ test($email);
+ test($password);
+ test($confirm_password);
+}
+
+
+function test($fieldName){
+    if (trim($fieldName) != ""){
+        echo "User $fieldName is NOT EMPTY<br>";
+        } else {
+        echo "Field $fieldName is EMPTY<br>";
+        }
+
+
+    }*/
 ?>
